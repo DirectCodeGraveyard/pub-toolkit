ec0df0e00231ff6b9376d693df7ee0bf6d6c5d1d
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-20 02:51:54 -0400

    Some Cleanup

--- lib/pub_pkgs.dart
+++ lib/pub_pkgs.dart
@@ -6,5 +6,6 @@ import "package:http/http.dart" as HTTP;
 
 part 'src/core.dart';
 part 'src/create_dump.dart';
+part 'src/statistics.dart';
 
 var http = new HTTP.Client();
---
+++ lib/src/statistics.dart
@@ -0,0 +1,1 @@
+part of pub.pkgs;
